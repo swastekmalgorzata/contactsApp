b4d42a93a135b62e5406627b234f45ab4db23a8b
Malgorzata Swastek <swastekm@gmail.com>
2023-04-13 22:35:46 +0200

    adding crud

--- ContactsApp/ContactsContext.cs
+++ ContactsApp/ContactsContext.cs
@@ -9,7 +9,7 @@ namespace ContactsApp
             : base (options)
             {
             }
-        DbSet<Contact> Contacts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
         
     }
 }
--- ContactsApp/ContanctsController.cs
+++ ContactsApp/ContanctsController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContactsApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +14,97 @@ namespace ContactsApp
     [ApiController]
     public class ContanctsController : ControllerBase
     {
+        private readonly ContactsContext _dbContext;
+        public ContanctsController(ContactsContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         // GET: api/<ContanctsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
         {
-            return new string[] { "value1", "value2" };
+
+            return await _dbContext.Contacts.ToListAsync();
         }
 
         // GET api/<ContanctsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Contact>> GetContact(int id)
         {
-            return "value";
+            if (_dbContext.Contacts == null) {
+                return NotFound();
+
+            }
+            var contact = await _dbContext.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return contact;
+            
         }
 
         // POST api/<ContanctsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Contact>> Post(Contact contact)
         {
+            _dbContext.Contacts.Add(contact);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(contact), new { id = contact.Id }, contact);
         }
 
         // PUT api/<ContanctsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult> Put(int id,Contact contact)
         {
+            if(id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(contact).State = EntityState.Modified;
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if (ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        private bool ContactExists(long id)
+        {
+            return (_dbContext.Contacts?.Any(e => e.Id == id)).GetValueOrDefault();
         }
 
         // DELETE api/<ContanctsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            if (_dbContext.Contacts == null)
+            {
+                return NotFound();
+
+            }
+
+            var contact = await _dbContext.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Contacts.Remove(contact);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
