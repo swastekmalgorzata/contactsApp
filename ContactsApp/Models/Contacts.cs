482558f7909cfa5c7a13e15aa49b8c9eeff05bff
Małgorzata Swastek <swastekm@gmail.com>
2023-04-13 11:40:03 +0200

    first commit

---
+++ ContactsApp/Models/Contacts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace ContactsApp.Models
+{
+	public class Contacts
+	{
+		public int Id { get; set; }
+
+		public string? Name { get; set; }
+
+		public string? Surname { get; set; }
+
+		public MailAddress? Email { get; set; }
+
+		public string? Password { get; set; }
+
+		public Enum? Category { get; set; }
+
+		public string? PhoneNumber { get; set; }
+
+		public DateOnly Birthdat { get; set; }
+
+	}
+}
+
